92ee6cf736b6f377358a052e17e9c01059557aef
Nikita Dudnik aka Nek <nikdudnik@gmail.com>
2013-04-28 17:04:35 +0700

    Lines version

--- game.js
+++ game.js
@@ -24,7 +24,6 @@ function start(){
         };
 }());
 
-//sc.use("prototype");
 
 function sequence(l) {
     var arr = new Array(l);
@@ -33,7 +32,27 @@ function sequence(l) {
     return arr;
 }
 
-var STEP = 50;
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+var STEP = 75;
+
+var BPM = 80;
+
+
+/*
+ *
+*
+*   Graphics system
+*
+*
+* */
 
 
 // create an new instance of a pixi stage
@@ -43,7 +62,7 @@ level.position.x = 0;
 level.position.y = 0;
 stage.addChild(level);
 // create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(400, 300);
+var renderer = PIXI.autoDetectRenderer(STEP*8, STEP*6);
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
@@ -52,15 +71,14 @@ var tx = 0;
 var ty = 0;
 
 
-var texture = PIXI.Texture.fromImage("box.png");
 
-var boxes = sequence(15000).map(function(v){
-    var box = new PIXI.Sprite(PIXI.Texture.fromImage("box.png"));
+var boxes = sequence(6*2).map(function(v){
+    var box = new PIXI.Sprite(PIXI.Texture.fromImage("line.png"));
     box.anchor.x = 0.5;
     box.anchor.y = 0.5;
-    box.position.x = 200;
-    box.position.y = v * STEP - 1000;
-    box.scale.x = box.scale.y = .5;
+    box.position.x = STEP*4;
+    box.position.y = v * STEP - STEP* (6/2);
+    box.scale.x = box.scale.y = 1;
     level.addChild(box);
     return box;
 })
@@ -68,57 +86,138 @@ var boxes = sequence(15000).map(function(v){
 var scope = new PIXI.Sprite(PIXI.Texture.fromImage("scope.png"));
 scope.anchor.x = .5;
 scope.anchor.y = .5;
-scope.position.x = 200;
-scope.position.y = 200;
+scope.position.x = STEP*4;
+scope.position.y = STEP*4;
 
 stage.addChild(scope);
 
-var BPM = 80;
 
-var bpmText = new PIXI.Text("BPM: "+BPM, "22px Arial", "red");
+var bpmText = new PIXI.Text("BPM"+BPM, "22px Arial", "red");
 bpmText.anchor.x = 0;
 bpmText.anchor.y = 1;
-bpmText.position.y = 300;
+bpmText.position.y = STEP*6;
 stage.addChild(bpmText);
 
 var scoreText = new PIXI.Text("0", "22px Arial", "red");
 scoreText.anchor.x = 1;
 scoreText.anchor.y = 1;
-scoreText.position.x = 400;
-scoreText.position.y = 300;
+scoreText.position.x = STEP*8;
+scoreText.position.y = STEP*6;
 stage.addChild(scoreText);
 
 var changes = [];
 var lbeat = 0;
 
-var beat = [1, 1, 1, 0, 1, 1, 1, 0];
+
+/*
+ *
+*
+*
+*   Sound system
+*
+*
+* */
+
+
+sc.use("prototype");
+
+var interv;
+
+
+function fits(v, base, span) {
+    return v > base - span && v < base + span;
+}
+
+function timeAt(timevalue) {
+    var l = timbre.timevalue(timevalue);
+    console.log( Math.round(interv.currentTime / l));
+    return Math.round(interv.currentTime / l)*l;
+}
+
 
 T("audio").load("drumkit.wav", function() {
-    var HH1 = this.slice(1000, 1500).set({bang:false, mul:0.2});
     var BD  = this.slice(   0,  500).set({bang:false});
-    var drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, HH1, BD).play();
-    var interv = T("interval", {interval:"BPM"+BPM+" L16"}, function(count) {
+    var SD  = this.slice( 500, 1000).set({bang:false});
+    var HH1 = this.slice(1000, 1500).set({bang:false, mul:0.2});
+    var HH2 = this.slice(1500, 2000).set({bang:false, mul:0.2});
+    var CYM = this.slice(2000).set({bang:false, mul:0.2});
+    var scale = new sc.Scale([0,1,3,7,8], 12, "Pelog");
+
+    var P2 = sc.series(16);
+
+
+    var drums = {BD:BD, SD:SD, HH1:HH1, HH2:HH2, CYM:CYM};
+    var lead = T("saw", {freq:T("param")});
+    var vcf  = T("MoogFF", {freq:2400, gain:6, mul:0.1}, lead);
+    var env  = T("perc", {r:150});
+    var arp  = T("OscGen", {wave:"sin(15)", env:env, mul:0.5});
+
+    // beats
+    var beat = {
+        "HH1":[1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+        "BD" :[0, 0, 0, 1, 0, 0, 0, 1],
+        "HH2" : [0,1,0,0],
+        "CYM": [0,0,0,0,1]
+//        ,"SD": [0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0]
+    };
+
+
+    T("delay", {time:"BPM128 L4", fb:0.65, mix:0.35},
+        T("pan", {pos:0.2}, vcf),
+        T("pan", {pos:T("tri", {freq:"BPM"+BPM/2+" L1", mul:0.8}).kr()}, arp)
+    ).play();
+
+    var drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, HH1, HH2, CYM, SD, BD).play();
+    window.interv = interv = T("interval", {interval:"BPM"+BPM+" L16"}, function(count) {
         lbeat = interv.currentTime;
-        scope.scale.x = scope.scale.y = 1.2;
-        if (sc.rotate(beat, count)[0] === 0) {
+        var keys = Object.keys(beat);
+        for (var i = 0; i < keys.length; i++) {
+            if (sc.rotate(beat[keys[i]], count)[0] === 1) drums[keys[i]].bang();
+        }
+        var noteNum = scale.wrapAt(P2.wrapAt(count)) + 60;
+        if (i % 2 === 0) {
+            lead.freq.linTo(noteNum.midicps() * 2, "100ms");
+        }
+        arp.noteOn(noteNum + 24, 60);
 
-           // scope.scale.x = scope.scale.y = 1 + .2;
-        } else HH1.bang();
+        // PAUSE logic
+        window.onblur = function() {
+            lead.set("mul", 0);
+            drum.set("mul", 0);
+            arp.set("mul", 0);
+        }
+
+        window.onfocus = function() {
+            lead.set("mul", 1);
+            drum.set("mul", 0.6);
+            arp.set("mul", 0.5);
+        }
+        // PAUSE end
+
     }).start();
+ // LOGICS START
+    function everySecond(t) {return fits(t, timeAt("BPM"+BPM+" L4"), 100);};
 
-    //var keydict = T("ndict.key");
-    //
-    //187 +
-    //189 -
-    //
-
-    function changeBPM(v) {
-        changes.push( {t:interv.currentTime, b:BPM} );
-        BPM = BPM + v;
-        interv.interval = "BPM"+BPM+" L16";
-        console.log("BPM: "+BPM);
-        bpmText.setText("BPM"+BPM);
+    var level1 = {
+        count: 0,
+        check: function(t) {
+            if (everySecond(t)) {
+                this.count ++;
+                SD.bang();
+            } else {
+                this.count = 0;
+            }
+            if (this.count === 8) {
+                changeBPM(5);
+                this.count = 0;
+            }
+        }
     }
+    function brain(t) {
+        level1.check(t);
+    }
+    // LOGICS END
+
 
     T("keyboard").on("keydown", function(e) {
         switch (e.which) {
@@ -129,34 +228,34 @@ T("audio").load("drumkit.wav", function() {
                 changeBPM(-20);
                 break;
             case 90:
-                //var nbeat = lbeat + 30000/(BPM*16)
-                //if (interv.currentTime < lbeat + 70) {
-                    BD.bang();
-                //}
+                brain(interv.currentTime);
                 break;
             default:
                 null;
         }
     }).start();
 
+     function changeBPM(v) {
+        changes.push( {t:interv.currentTime, b:BPM} );
+        BPM = BPM + v;
+        interv.interval = "BPM"+BPM+" L16";
+        bpmText.setText("BPM"+BPM);
+    }
+
     function foldPos(res, c, i, a)
     {
-        console.log("r",res,"c",c,"i",i);
-        console.log((res.t));
-        console.log((c.t));
         return {t:c.t, b: c.b, s: res.s + (res.t - c.t)*res.b + (i === 0 ? c.b : 0) };
     }
 
     function animate() {
+        requestAnimFrame(animate);
         var move = 0;
         if (changes.length > 0) {
             //console.log(changes);
             var s = changes.reduceRight(foldPos, {t: interv.currentTime, b: BPM, s: 0}).s;
-            console.log(s);
             move = s*STEP/30000;
         } else move = interv.currentTime * (BPM*STEP/30000);
-        level.position.y = move;
-        requestAnimFrame(animate);
+        level.position.y = move%(STEP*(6/2));
         renderer.render(stage);
     }
 
